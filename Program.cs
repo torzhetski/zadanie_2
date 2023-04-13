161319ec3c5d6251c80fd8af7149b2fe80d7cc0b
5-704-4 <5-704-4@DESKTOP-MAJG90J>
2023-04-13 13:49:34 +0300

    добавлен ручной ввод

--- Program.cs
+++ Program.cs
@@ -12,21 +12,47 @@ class Program
     {
         List<Stage> project = new List<Stage>();
         WriteLine("выберети форму ввода:\n1 - ручной ввод\n2 - автоматический ввод");
-        //ConsoleKeyInfo key = ReadKey();
-        //Clear();
-        //switch (key.KeyChar)
-        //{
-        //    case '1':
+        ConsoleKeyInfo key = ReadKey();
+        Clear();
+        switch (key.KeyChar)
+        {
+            case '1':
 
-        //        WriteLine("Стадия Анализирования:\nВведите число сотрудников:");
-                
-        //        Analizing analizing1 = new Analizing(0, 0, 0);
-        //        WriteLine("Введите финансы на стадию:");
-        //        break;
-        //    default:
-        //        WriteLine("Введен некорректный символ, заполнено автоматически");
-        //        break;
-        //    case '2':
+                WriteLine("Стадия Анализирования:\nВведите число сотрудников:");
+                uint number = Convert.ToUInt32(ReadLine());
+                Analizing analizing1 = new Analizing(0, 0, number);
+                analizing1 = (Analizing)Fill(analizing1);
+                WriteLine("Введите сферу проекта:");
+                analizing1.NameOfSphere = ReadLine();
+                WriteLine("Стадия Проектирования:\nВведите число сотрудников:");
+                 number = Convert.ToUInt32(ReadLine());
+                Projecting projecting1 = new Projecting(0, 0, number);
+                projecting1 = (Projecting)Fill(projecting1);
+                WriteLine("Введите количество файлов проекта:");
+                projecting1.NumberOfFiles = Convert.ToUInt32(ReadLine());
+                WriteLine("Стадия Кодирования:\nВведите число сотрудников:");
+                 number = Convert.ToUInt32(ReadLine());
+                Coding coding1 = new Coding(0, 0, number);
+                coding1 = (Coding)Fill(coding1);
+                WriteLine("Введите количество строк кода:");
+                coding1.NumberOfLines = Convert.ToUInt32(ReadLine());
+                WriteLine("Стадия Тестирования:\nВведите число сотрудников:");
+                 number = Convert.ToUInt32(ReadLine());
+                Testing testing1 = new Testing(0, 0, number);
+                testing1 = (Testing)Fill(testing1);
+                WriteLine("Введите количество найденных багов:");
+                testing1.NumberOfBugs = Convert.ToUInt32(ReadLine());
+                WriteLine("Стадия экспулатации:\nВведите число сотрудников:");
+                 number = Convert.ToUInt32(ReadLine());
+                Expluatation expluatation1 = new Expluatation(0, 0, number);
+                expluatation1 = (Expluatation)Fill(expluatation1);
+                WriteLine("Введите год до которого будет осуществляться поддержка:");
+                expluatation1.SupportPeriod = new DateTime(Convert.ToInt32(ReadLine()),1,1);
+                break;
+            default:
+                WriteLine("Введен некорректный символ, заполнено автоматически");
+                break;
+            case '2':
                 Analizing analizing = new Analizing(10,5000,5);
                 Projecting projecting = new Projecting(10,5000,5);
                 Coding coding = new Coding(10,5000,5);
@@ -36,10 +62,18 @@ class Program
                 foreach (var item in project)
                     WriteLine(item);
                 WriteLine($"Итого затрачено на проект:{project[0].ExpensesForProject(project)}");
-                //break;
+                break;
 
-        //}
+        }
+    }
 
-
+    public static Stage Fill(Stage stage)
+    {
+        WriteLine("Введите срок выполнения стадии:");
+        stage.DueTime = Convert.ToUInt32(ReadLine());
+        WriteLine("Введите финансы на стадию:");
+        stage.Finance = Convert.ToUInt32(ReadLine());
+        stage.Team = 0;
+        return stage;
     }
 }
